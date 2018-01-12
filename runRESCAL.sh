7d526c120eb041bb12eab4e770a81b304544e3cd
Jake Lever <jlever@bcgsc.ca>
2018-01-12 06:12:59 -0800

    Change RESCAL outpit

--- runRESCAL.sh
+++ runRESCAL.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euxo pipefail
 
 rank=$1
 lambda_A=$2
@@ -10,6 +11,6 @@ mkdir -p intermediate
 mkdir -p crossvalidated
 
 python predict_rescal.py --trainingData data/validation.train.tsv --testingData data/validation.test.tsv --outFile intermediate/rescal_$name --rank $rank --lambda_A $lambda_A --lambda_R $lambda_R
-python evaluate.py --data intermediate/rescal_$name --classbalance 0.000131542 > crossvalidated/out_$name
+python evaluate.py --data intermediate/rescal_$name --classbalance 0.000131542 > crossvalidated/rescal_$name
 
 
